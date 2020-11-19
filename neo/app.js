2f4f705e746eb98ee55f32eec6155a95fc71e9ce
thedarkdivinity <sayush.kamat@rediffmail.com>
2020-11-20 00:25:20 +0530

    refactored neo folder to give json response

--- neo/app.js
+++ neo/app.js
@@ -1,180 +1,139 @@
-var express=require('express');
-var path=require('path');
-var logger=require('morgan');
-var cookieParser=require('cookie-parser');
-var bodyParser=require('body-parser');
-var app=express();
-var neo4j=require('neo4j-driver');
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const app = express();
+const neo4j = require("neo4j-driver");
 //view engine setup
-app.set('views',path.join(__dirname,'views'));
-app.set('view engine','ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 //middleware
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-var driver=neo4j.driver("bolt://localhost",neo4j.auth.basic('neo4j','neo5j'));
-var session =driver.session();
+const driver = neo4j.driver(
+  "bolt://localhost",
+  neo4j.auth.basic("neo4j", "sayush")
+);
+const session = driver.session();
 //home route
 
-app.get('/',function(req,res){
+app.get("/",  async function (req, res) {
+  const users=await session.run("MATCH (n:user) RETURN n");
+  res.status(200).json(users);
+    
 
-    session
-        .run("MATCH (n:user) RETURN n")
-        .then(function(result){
-            var userArr=[];
-            result.records.forEach(function(record){
-                //console.log(record._fields[0]);
-                userArr.push({
-                    id:record._fields[0].identity.low,
-                    fname:record._fields[0].properties.fname
-                });
-            });
-            session
-            .run("MATCH (n:place) RETURN n")
-            .then(function(result2){
-                var placeArr=[];
-                result2.records.forEach(function(record){
-                    //console.log(record._fields[0]);
-                    placeArr.push(record._fields[0].properties);
-                    });   
-        
-            res.render('index',{
-                users:userArr,
-                places :placeArr
-            });
-        })
-    })
-        .catch(function(error){
-            console.log(error);
-        });
-   // res.render('index');
 });
+app.get("/places",async(req,res)=>{
+  
+        const places=await session.run("MATCH (n:place) RETURN n");
+        res.status(200).json(places);
+
+    
+    
+})
 
 //add user route
 
-app.post('/user/add',function(req,res){
-    var uid=req.body.uid;
-    var uname=req.body.uname;
-    var uemail=req.body.uemail;
-    var ulat=req.body.ulat;
-    var ulong=req.body.ulong;
+app.post("/user/add", async function (req, res) {
+  const { uid, uname, uemail, ulat, ulong } = req.body;
 
-    //console.log(name);
-    session
-     .run("CREATE (n:user {uid:$uidp,uname:$unamep,uemail:$uemailp,ulat:$ulatp,ulong:$ulongp}) RETURN n", {uidp:uid,unamep:uname,ulatp:ulat,ulongp:ulong,uemailp:uemail})
+ const newUser= await  session
+    .run(
+      "CREATE (n:user {uid:$uid,uname:$uname,uemail:$uemail,ulat:$ulat,ulong:$ulong}) RETURN n",
+      { uid, uname, ulat, ulong, uemail }
+    );
+    res.status(200).json(newUser);
 
-    .then(function(result){
-        res.redirect('/');
-       session.close();
-        })
-
-    .catch(function(error){
-        console.log(error);
-    });
 });
 //add place route
-app.post('/place/add',function(req,res){
-    var pid=req.body.pid;
-    var pname=req.body.pname;
-    var prating=req.body.prating;
-    var padd=req.body.padd;
-    var plat=req.body.plat;
-    var plong=req.body.plong;
-    var ptype=req.body.ptype;
-    //console.log(name);
-    session
-     .run("CREATE (n:place {pid:$pidp,pname:$pnamep,plat:$platp,plong:$plongp,padd:$paddp,prating:$pratingp,ptype:$ptypep}) RETURN n", {pidp:pid,pnamep:pname,pratingp:prating,ptypep:ptype,platp:plat,plongp:plong,paddp:padd})
+app.post("/place/add", async function (req, res) {
+  const { pid, pname, prating, padd, plat, plong, ptype } = req.body;
 
-    .then(function(result){
-        res.redirect('/');
-       session.close();
-        })
-        
-    .catch(function(error){
-        console.log(error);
-    });
+  //console.log(name);
+ const place=  await session
+    .run(
+      "CREATE (n:place {pid:$pid,pname:$pname,plat:$plat,plong:$plong,padd:$padd,prating:$prating,ptype:$ptype}) RETURN n",
+      {
+        pid,
+        pname,
+        prating,
+        ptype,
+        plat,
+       plong,
+        padd,
+      }
+    );
+    res.status(200).json(place);
 });
 
 //frnds connect route
-app.post('/friends/connect',function(req,res){
-    var uid1=req.body.uid1;
-    var uid2=req.body.uid2;
-    console.log(uid1);
-    console.log(uid2);
-    session
-     .run("MATCH(a:user {uid:$uid1p}),(b:user {uid:$uid2p}) MERGE(a)-[r:friends]->(b) RETURN r",{uid1p:uid1,uid2p:uid2})
+app.post("/friends/connect",  async function (req, res) {
+  const { uid1, uid2 } = req.body;
 
-    .then(function(result){
-        res.redirect('/');
-      //  session.close();
-        })
-        
-    .catch(function(error){
-        console.log(error);
-    });
+  const connection= await session
+    .run(
+      "MATCH(a:user {uid:$uid1}),(b:user {uid:$uid2}) MERGE(a)-[r:friends]->(b) RETURN r",
+      {uid1,uid2 }
+    );
+    res.status(200).json(connection);
+
+   
 });
 //connect place to place
-app.post('/placedist/connect',function(req,res){
-    var pid1=req.body.pid1;
-    var pid2=req.body.pid2;
-    console.log(pid1);
-    console.log(pid2);
-    session
-     .run("MATCH(a:place {pid:$pid1p}),(b:place {pid:$pid2p}) MERGE(a)-[r:distance {dist:distance(point({latitude:toFloat(a.plat),longitude:toFloat(a.plong)}),point({latitude:toFloat(b.plat),longitude:toFloat(b.plong)}))}]->(b) RETURN r",{pid1p:pid1,pid2p:pid2})
+app.post("/placedist/connect",  async function (req, res) {
+  const { pid1, pid2 } = req.body;
 
-    .then(function(result){
-        res.redirect('/');
+   session
+    .run(
+      "MATCH(a:place {pid:$pid1}),(b:place {pid:$pid2}) MERGE(a)-[r:distance {dist:distance(point({latitude:toFloat(a.plat),longitude:toFloat(a.plong)}),point({latitude:toFloat(b.plat),longitude:toFloat(b.plong)}))}]->(b) RETURN r",
+      { pid1, pid2 }
+    )
+
+    .then(function (result) {
+      res.redirect("/");
       session.close();
-        })
-        
-    .catch(function(error){
-        console.log(error);
+    })
+
+    .catch(function (error) {
+      console.log(error);
     });
 });
 //connect user to place
-app.post('/userratesplace/connect',function(req,res){
-    var uid=req.body.uid;
-    var pid=req.body.pid;
-    var rating =req.body.rating;
-    var rid=req.body.rid;
-    console.log(uid);
-    console.log(pid);
-    session
-     .run("MATCH(a:user {uid:$uidp}),(b:place {pid:$pidp}) MERGE(a)-[r:rates {rating:$ratingp,rid:$ridp} ]->(b) RETURN r",{uidp:uid,pidp:pid,ratingp:rating,ridp:rid})
+app.post("/userratesplace/connect", async function (req, res) {
+  const { uid, pid, rating, rid } = req.body;
 
-    .then(function(result){
-        res.redirect('/');
-        session.close();
-        })
-        
-    .catch(function(error){
-        console.log(error);
-    });
+
+  const UserRatePlaces= await  session
+    .run(
+      "MATCH(a:user {uid:$uid}),(b:place {pid:$pid}) MERGE(a)-[r:rates {rating:$rating,rid:$rid} ]->(b) RETURN r",
+      { uid, pid, rating, rid }
+    );
+    res.status(200).json(UserRatePlaces);
+
+    
 });
-app.post('/placerecommendation',function(req,res){
-    var r=req.body.r;
-    var ptype=req.body.ptype;
-    //console.log(name);
-    session
-     .run("MATCH(me:user)-[:friends]->(f),(f)-[r:rates]-(p:place) WHERE r.rating > 3 AND NOT (me)-[:rates]->(p) AND p.ptype=$type AND distance(point({latitude:toFloat(me.ulat),longitude:toFloat(me.ulong)}),point({latitude:toFloat(p.plat),longitude:toFloat(p.plong)})) < $radius RETURN distinct p AS place,count(*) AS count  ORDER BY count DESC LIMIT 10", {radius:r,type:ptype})
+app.post("/placerecommendation", async function (req, res) {
 
-    .then(function(result){
-        res.redirect('/');
-       session.close();
-        })
+    const {r,ptype}=req.body;
+ 
+  //console.log(name);
+ const placerecommendation= await session
+    .run(
+      "MATCH(me:user)-[:friends]->(f),(f)-[r:rates]-(p:place) WHERE r.rating > 3 AND NOT (me)-[:rates]->(p) AND p.ptype=$type AND distance(point({latitude:toFloat(me.ulat),longitude:toFloat(me.ulong)}),point({latitude:toFloat(p.plat),longitude:toFloat(p.plong)})) < $radius RETURN distinct p AS place,count(*) AS count  ORDER BY count DESC LIMIT 10",
+      { radius: r, type: ptype }
+    )
 
-    .catch(function(error){
-        console.log(error);
-    });
+  res.status(200).json(placerecommendation);
 });
-
 
 app.listen(9000);
 
-console.log('Server started on port 9000');
+console.log("Server started on port 9000");
 
-module.export=app;
+module.export = app;
